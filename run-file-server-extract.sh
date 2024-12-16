6f1c3679964b76c57283bc53fba96f865703c33d
Florian Begusch <florian.begusch@gmail.com>
2024-12-16 20:30:15 +0000

    Fix extract for default lima

--- run-file-server-extract.sh
+++ run-file-server-extract.sh
@@ -8,9 +8,16 @@ set -e  # exit on non-zero return value
 shopt -s failglob  # error on unexpaned globs
 shopt -s inherit_errexit  # Bash disables set -e in command substitution by default; reverse this behavior
 
+
+
+path=~/.colima/_lima/colima/ssh.config
+if [ "${DOCKER_BACKING_INSTANCE:=}" = default ]; then
+  path=~/.lima/default/ssh.config
+fi
+
 if [ "$1" = id_file ]; then
-  grep IdentityFile ~/.colima/_lima/colima/ssh.config | awk -F '"' '{ print $2 }'
+  grep IdentityFile "$path" | awk -F '"' 'NR==1 { print $2; exit }'
 elif [ "$1" = port ]; then
-  grep Port ~/.colima/_lima/colima/ssh.config | awk '{ print $2 }'
+  grep Port "$path" | awk 'NR==1 { print $2; exit }'
 fi
 
